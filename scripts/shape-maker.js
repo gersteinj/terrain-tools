b54066b87fa8e7e24a0631b696bb99d2fcb74082
gersteinj <gersteinj@gmail.com>
2018-05-13 22:40:05 -0400

    reconfigured with modals to maximize canvas size

--- scripts/shape-maker.js
+++ scripts/shape-maker.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('.modal').modal(open())
 });
 
 $( "#export-button" ).click(function(){
